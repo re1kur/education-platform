b010272c1828cf45c2ece1429c5e7dbb9d8a9d1e
re1kur <re1kur@yahoo.com>
2025-10-19 13:30:48 +0700

    looks like done v2/ locally tested

--- task-service/src/main/java/com/example/taskservice/config/SecurityConfig.java
+++ task-service/src/main/java/com/example/taskservice/config/SecurityConfig.java
@@ -34,7 +34,9 @@ public class SecurityConfig {
                                 "/swagger-ui.html",
                                 "/v3/api-docs",
                                 "/v3/api-docs/**",
-                                "/actuator/**").permitAll()
+                                "/actuator/**",
+                                "/api/v1/outbox",
+                                "/api/v1/outbox/**").permitAll()
                         .requestMatchers("/api/v1/test/token",
                                 "/api/v1/tasks/attempts/*/results").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/api/v1/tasks/*").hasRole("ADMIN")
